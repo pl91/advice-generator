c6107a1e852ac07212565cfd29d7faba042a6056
Paul Liani <pliani91@gmail.com>
2023-01-27 16:19:16 -0500

    .

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// const advice = document.querySelector("#advice");
-// const button = document.querySelector("button");
-
-// const addAdvice = async () => {
-//   const adviceText = await getAdvice();
-//   const newP = document.createElement("p");
-//   newP.append(adviceText);
-//   advice.append(newP);
-// };
-
-// const getAdvice = async () => {
-//   try {
-//   const res = await axios.get("https://api.adviceslip.com/advice");
-//   return res.data.slip.advice;}
-
-//   catch (e) {
-//     return "NO ADVICE AVAILABLE!";
-//   }
-// };
-
-// button.addEventListener("click", addAdvice);
-
 const adviceText = document.querySelector("#advice-text");
 const adviceId = document.querySelector("#advice-id");
 const button = document.querySelector("#dice-icon");
